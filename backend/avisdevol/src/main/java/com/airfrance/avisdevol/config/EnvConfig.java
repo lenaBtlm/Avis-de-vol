2251ccccf15706dbc4c110274f2adf1796b47df6
lbitch-barthelemy <lena.bitch-barthelemy@amadeus.com>
2025-06-23 09:56:53 +0200

    BE - add security for conexion with bdd

---
+++ backend/avisdevol/src/main/java/com/airfrance/avisdevol/config/EnvConfig.java
@@ -0,0 +1,17 @@
+package com.airfrance.avisdevol.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
+    
+}
